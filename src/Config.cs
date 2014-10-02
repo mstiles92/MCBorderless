3e71177c268f38ea65af2932dc1ed5e986ca9de2
mstiles92 <mstiles92@gmail.com>
2014-10-02 18:33:14 -0400

    Remove JSON library and all usages of it.

--- src/Config.cs
+++ src/Config.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.IO;
 using System.Windows.Forms;
 
@@ -11,24 +10,6 @@ namespace MCBorderless {
         public Config(string[] windowTitleContents, string[] windowTitleExclusions) {
             this.WindowTitleContents = windowTitleContents;
             this.WindowTitleExclusions = windowTitleExclusions;
-        }
-
-        public static Config loadFromJson(string filename) {
-            if (!File.Exists(filename)) {
-                string[] windowTitleContents = new string[] { "Minecraft" };
-                string[] windowTitleExclusions = new string[] { "Launcher" };
-                Config Config = new Config(windowTitleContents, windowTitleExclusions);
-                string json = JsonConvert.SerializeObject(Config);
-                StreamWriter writer = new StreamWriter(File.OpenWrite(filename));
-                writer.Write(json);
-                writer.Close();
-                return Config;
-            } else {
-                StreamReader reader = new StreamReader(File.OpenRead(filename));
-                string json = reader.ReadToEnd();
-                reader.Close();
-                return JsonConvert.DeserializeObject<Config>(json);
-            }
         }
 
         public static Config loadFromRegistry() {
--- src/Program.cs
+++ src/Program.cs
@@ -6,20 +6,8 @@ using System.Windows.Forms;
 namespace MCBorderless {
     static class Program {
 
-        internal static Assembly AssemblyResolveHandler(object sender, ResolveEventArgs args) {
-            if (args.Name.Contains("Newtonsoft") && args.Name.Contains("Json") && !args.Name.EndsWith("_resources")) {
-                ResourceManager rm = new ResourceManager("MCBorderless.Properties.Resources", Assembly.GetExecutingAssembly());
-                byte[] bytes = (byte[]) rm.GetObject("Newtonsoft_Json");
-                return Assembly.Load(bytes);
-            } else {
-                return null;
-            }
-        }
-
         [STAThread]
         static void Main() {
-            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolveHandler;
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Keybinding.Hook();
