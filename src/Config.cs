fc1b797b747f4c4868d610664b381964e0887a4e
mstiles92 <mstiles92@gmail.com>
2014-10-02 17:43:12 -0400

    Remove launcher filename setting from Config class, as it is no longer used.

--- src/Config.cs
+++ src/Config.cs
@@ -3,22 +3,19 @@ using System.IO;
 
 namespace MCBorderless {
     class Config {
-        public string LauncherFilename { get; private set; }
         public string[] WindowTitleContents { get; private set; }
         public string[] WindowTitleExclusions { get; private set; }
 
-        public Config(string launcherFilename, string[] windowTitleContents, string[] windowTitleExclusions) {
-            this.LauncherFilename = launcherFilename;
+        public Config(string[] windowTitleContents, string[] windowTitleExclusions) {
             this.WindowTitleContents = windowTitleContents;
             this.WindowTitleExclusions = windowTitleExclusions;
         }
 
         public static Config loadFromJson(string filename) {
             if (!File.Exists(filename)) {
-                string launcherFilename = "Minecraft.exe";
                 string[] windowTitleContents = new string[] { "Minecraft" };
                 string[] windowTitleExclusions = new string[] { "Launcher" };
-                Config Config = new Config(launcherFilename, windowTitleContents, windowTitleExclusions);
+                Config Config = new Config(windowTitleContents, windowTitleExclusions);
                 string json = JsonConvert.SerializeObject(Config);
                 StreamWriter writer = new StreamWriter(File.OpenWrite(filename));
                 writer.Write(json);
